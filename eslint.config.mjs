9868487b17df9ec4f051edb140485daba612f372
jayv1ins <nakpil47@gmail.com>
2025-07-09 21:16:15 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ✅ Add custom rule override
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
